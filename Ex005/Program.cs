2d265d7fe64496f89484026410fde0c0479c4f62
Sergey Pochikaev <psvakm@gmail.com>
2023-01-06 19:39:15 +0700

    Пример 5_Сравнение имен

---
+++ Ex005/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите ваше имя: ");
+string UserName = Console.ReadLine();
+//String Маша;
+if (UserName.ToLower() == "Маша")
+  {
+  Console.WriteLine("Привет Маша!!!");
+  }
+else
+  {
+  Console.Write("Привет ");
+  Console.WriteLine(UserName);
+  }
