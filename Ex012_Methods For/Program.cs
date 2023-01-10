d44e1d5c47a20e90e6ad9a38a2c4f306a5e1dfe4
Sergey Pochikaev <psvakm@gmail.com>
2023-01-10 22:18:00 +0700

    Описаны 4 метода по передачи аргументов, расмотренно Цикл в Цикле с примерами

---
+++ Ex012_Methods For/Program.cs
@@ -0,0 +1,25 @@
+﻿// Вид 4 (Что-то принимают и выдают но с процедурой цикла for)
+string Method4(int count, string text);
+{
+
+  string result = ""; //Пустая строка String.Empty
+  for (int i = 0; i < count; i++)
+  {
+    result = result + text;
+  }
+  return result;
+}
+// Вызываются Method4
+string res = Method4(10, "asdf");
+Console.WriteLine(res);
+
+
+//Пример//(Вывод табл. умножения через for)
+  for (int i = 2; i <= 10; i++)
+  {
+    for (int j = 2; j <= 10; j++)
+    {
+      Console.WriteLine($"{i} * {j}  = {i*j}");//Использовано интрополяция строк i*j
+    }
+    Console.WriteLine();
+  }
---
+++ Ex012_Methods/Program.cs
@@ -0,0 +1,66 @@
+﻿// Вид 1 (Не чего не возвращают и не чего не принимают)
+void Method1()
+{
+  Console.WriteLine("Автор ....");
+}
+// Вызываются Method1
+Method1();
+
+
+
+
+// Вид 2 (Не чего не возвращают, но принимают аргументы)
+void Method2(string msg)
+{
+  // выводит сообщение
+  Console.WriteLine(msg);
+}
+// Вызываются Method2
+Method2("Текст сообщения");
+// можно указать конкретное значение присвоить аргумент, если метод принимает какое-то кол-во аргументов
+Method2(msg: "Текст сообщения");
+//Например://
+void Method2(string msg, int count)
+{
+  int i = 0;
+  while (i < count)
+  {
+    Console.WriteLine(msg);
+    i++;
+  }
+}
+//Вызов Method2//
+Method2("Текст", 4);
+//Если конкретно из списков аргументов то можно пристроить://
+Method2(msg: "Текст", count: 4); // Аргументы можно менять местами
+
+
+
+
+// Вид 3 (Что-то возвращают, но ни чего не принимают)
+int Method3();
+{
+  return DateTime.Now.Year;
+}
+// Вызываются Method3
+int year = Method3();
+Console.WriteLine(year);
+
+
+
+
+// Вид 4 (Что-то принимают и выдают)
+string Method4(int count, string text);
+{
+  int i = 0;
+  string result = ""; //Пустая строка String.Empty
+  while (i < count)
+  {
+    result = result + text;
+    i++;
+  }
+  return result;
+}
+// Вызываются Method4
+string res = Method4(10, "asdf");
+Console.WriteLine(res);
